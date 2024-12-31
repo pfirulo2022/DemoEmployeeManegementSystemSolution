9536ecac918651fd89cdbc1806c67dcd69ab0e5c
Pablo Firulo <pfirulo@yahoo.com>
2024-12-31 17:17:49 -0300

    token complete

--- Server/Controllers/AuthenticationController.cs
+++ Server/Controllers/AuthenticationController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿
+using BaseLibrary.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using ServerLibrary.Repositories.Contracts;
 
 namespace Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthenticationController : ControllerBase
+    public class AuthenticationController(IUserAccount accountInterface) : ControllerBase
     {
+        [HttpPost("register")]
+        public async Task<IActionResult> CreateAsync(Register user)
+        {
+            if (user == null) return BadRequest("Model is empty");
+            var result = await accountInterface.CreateAsync(user);
+            return Ok(result);
+
+        }
+
     }
 }
--- ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
+++ ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
@@ -3,9 +3,13 @@ using BaseLibrary.Entities;
 using BaseLibrary.Responses;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using ServerLibrary.Data;
 using ServerLibrary.Helpers;
 using ServerLibrary.Repositories.Contracts;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace ServerLibrary.Repositories.Implementations
 {
@@ -29,10 +33,10 @@ namespace ServerLibrary.Repositories.Implementations
             // check, create and assign role
             var checkAdminRole = await appDbContext.SystemRoles.FirstOrDefaultAsync(_ => _.Name!.Equals(Constants.Admin));
 
-            if(checkAdminRole is null)
+            if (checkAdminRole is null)
             {
-                var createAdminRole = await AddToDatabase(new SystemRole() { Name = Constants.Admin});
-                await AddToDatabase(new UserRole() { RoleId = createAdminRole.Id,UserId = applicationUser.Id});
+                var createAdminRole = await AddToDatabase(new SystemRole() { Name = Constants.Admin });
+                await AddToDatabase(new UserRole() { RoleId = createAdminRole.Id, UserId = applicationUser.Id });
                 return new GeneralResponse(true, "Account created successfully");
             }
 
@@ -43,7 +47,8 @@ namespace ServerLibrary.Repositories.Implementations
             {
                 response = await AddToDatabase(new SystemRole() { Name = Constants.User });
                 await AddToDatabase(new UserRole() { RoleId = response.Id, UserId = applicationUser.Id });
-            }else
+            }
+            else
             {
                 await AddToDatabase(new UserRole() { RoleId = checkUserRole.Id, UserId = applicationUser.Id });
             }
@@ -53,19 +58,69 @@ namespace ServerLibrary.Repositories.Implementations
 
 
 
-        private async Task<ApplicationUser> FindUserByEmail(string email)
+        private async Task<ApplicationUser> FindUserByEmail(string email)   
             => await appDbContext.ApplicationUsers.FirstOrDefaultAsync(_ => _.Email!.ToLower()!.Equals(email!.ToLower()));
 
 
-        public Task<LoginResponse> SignInAsync(Login user)
+        public async Task<LoginResponse> SignInAsync(Login user)
+        {
+            if (user is null) return new LoginResponse(false, "Model is empty");
+
+            var applicationUser = await FindUserByEmail(user.Email!);
+            if (applicationUser is null) return new LoginResponse(false, "User not found");
+
+            // verify password
+            if (!BCrypt.Net.BCrypt.Verify(user.Password!, applicationUser.Password!))
+                return new LoginResponse(false, "Invalid credentials");
+
+            // get user role
+            var getUserRole = await appDbContext.UserRoles.FirstOrDefaultAsync(_ => _.UserId == applicationUser.Id);
+
+            if (getUserRole is null) return new LoginResponse(false, "User role not found");
+
+            var getRoleName = await appDbContext.SystemRoles.FirstOrDefaultAsync(_ => _.Id == getUserRole.RoleId);
+
+            if (getUserRole is null) return new LoginResponse(false, "User role not found");
+
+            string jwtToken = GenerateToken(applicationUser, getRoleName!.Name!);
+            string refreshToken = GenerateRefreshToken();
+            return new LoginResponse(true, "Login successfully", jwtToken, refreshToken);
+        }
+
+        private string GenerateRefreshToken(ApplicationUser user, string role)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.Value.Key!));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var userClaims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Fullname!),
+                new Claim(ClaimTypes.Email, user.Email!),
+                new Claim(ClaimTypes.Role, role)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: config.Value.Issuer,
+                audience: config.Value.Audience,
+                claims: userClaims,
+                expires: DateTime.Now.AddDays(5),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private string GenerateToken(ApplicationUser applicationUser, string v)
         {
             throw new NotImplementedException();
         }
 
-        
+
+
+
         private async Task<T> AddToDatabase<T>(T model)
         {
-             var result = appDbContext.Add(model!);
+            var result = appDbContext.Add(model!);
             await appDbContext.SaveChangesAsync();
             return (T)result.Entity;
         }
