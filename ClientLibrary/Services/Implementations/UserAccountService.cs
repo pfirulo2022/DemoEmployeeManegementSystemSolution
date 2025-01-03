e02435a3b38e9efa8676057aea04ed33ea61b7f5
Pablo Firulo <pfirulo@yahoo.com>
2025-01-03 19:19:34 -0300

    implementando cliente

---
+++ ClientLibrary/Services/Implementations/UserAccountService.cs
@@ -0,0 +1,30 @@
+﻿
+using BaseLibrary.DTOs;
+using BaseLibrary.Responses;
+using ClientLibrary.Services.Contracts;
+
+namespace ClientLibrary.Services.Implementations
+{
+    public class UserAccountService : IUserAccountService
+    {
+        public Task<GeneralResponse> CreateAsync(Register user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<WeatherForecast> GetWeatherForecast()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponse> RefreshTokenAsync(RefreshToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponse> SignInAsync(Login user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
